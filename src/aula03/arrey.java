8a93528bb0425afc3275e7a5cfaed6dc05453a37
narely <narelyalbuquerque25@gmail.com>
2025-06-12 16:26:06 -0300

    java

---
+++ src/aula03/arrey.java
@@ -0,0 +1,14 @@
+package aula03;
+
+public class arrey {
+
+	public static void main(String[] args) {
+	int numeros []= {2,3,5,8,7};
+	double notas[]= new double[4];
+	for (int x = 0 ; x < notas.length;x++) {
+		System.out.println(notas[x]);
+	}
+
+	}
+
+}
